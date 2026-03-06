c23f888b0d44b6e462febd0969b84105a4bd7967
varshini <jonnalagaddavarshini3@gmail.com>
2026-03-06 09:31:28 +0530

    Initial commit

---
+++ src/main/java/com/example/cseghrender/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.cseghrender;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users, Integer>{
+    Users findByEmail(String email);
+
+}
